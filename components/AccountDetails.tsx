d76585ce52c0e941c779968be50c93a3b0bc3f9f
atude <mozamel.anwary1@gmail.com>
2019-11-21 14:56:50 +1100

    Testing with react hooks instead of classes

--- components/AccountDetails.tsx
+++ components/AccountDetails.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, Text, Button } from 'react-native';
 
 import { ethers } from 'ethers';
@@ -6,39 +6,25 @@ import { ethers } from 'ethers';
 const MY_ADDRESS = "0xF1008457018f72e4BF1dA477273ec9A7Ac78D253";
 const FAUCET_ADDRESS = "0x81b7e08f65bdf5648606c89998a9cc8164397647";
 
-export default class AccountDetails extends React.Component {
-  state = {
-    balance: "0",
-    faucetBalance: "0",
-    isProcessing: null,
-    network: ethers.getDefaultProvider('ropsten').network.name,
-    provider: ethers.getDefaultProvider('ropsten')
-  }
+export const AccountDetails = () => {
+  const [balance, setBalance] = useState("0");
+  const [faucetBalance, setFaucetBalance] = useState("0");
+  const [isProcessing, setIsProcessing] = useState(null);
+  
+  const network = ethers.getDefaultProvider('ropsten').network.name;
+  const provider = ethers.getDefaultProvider('ropsten');
 
-  async componentDidMount() {
-    this.updateBalance();
-  }
+  useEffect(() => {
+    async function updateBalances() {
+      setBalance(await provider.getBalance(MY_ADDRESS).then(balance => balance.toString())); 
+      setFaucetBalance(await provider.getBalance(FAUCET_ADDRESS).then(balance => balance.toString())); 
+    }
 
-  async updateBalance() {
-    const { provider } = this.state;
+    updateBalances();
+  })
 
-    const balance = await provider.getBalance(MY_ADDRESS)
-      .then(balance => balance.toString()); 
-
-    const faucetBalance = await provider.getBalance(FAUCET_ADDRESS)
-      .then(balance => balance.toString()); 
-
-    this.setState({
-      balance: balance,
-      faucetBalance: faucetBalance
-    });
-  }
-
-  sendTransaction(amount: string, address: string) {
-    const { provider } = this.state;
-    this.setState({
-      isProcessing: true
-    })
+  const sendTransaction = (amount: string, address: string) => {
+    setIsProcessing(true);
 
     let privateKey = '0x7D1CBDDDFB8356594DC69AF971723E2697976CFA1BC4B8871A7184CC3CDB1305';
     let wallet = new ethers.Wallet(privateKey, provider);
@@ -53,37 +39,32 @@ export default class AccountDetails extends React.Component {
 
     sendTransactionPromise.then((tx) => {
       console.log(tx);
-      this.updateBalance();
-      this.setState({isProcessing: false})
+      setIsProcessing(false);
     });
   }
       
-  render() {
-    let { network, balance, faucetBalance, isProcessing } = this.state;
+  return (
+    <>
+      {isProcessing == null && 
+        <Text style={{color: "red"}}> --- </Text>
+      }
 
-    return (
-      <>
-        {isProcessing == null && 
-          <Text style={{color: "red"}}> --- </Text>
-        }
-
-        {isProcessing === true &&
-          <Text style={{color: "red"}}>Processing transaction...</Text>
-        } 
-        
-        {isProcessing === false &&
-          <Text style={{color: "green"}}>Transaction Completed.</Text>
-        }
-        <Text>Network: {network}</Text>
-        <Text>My Address: {MY_ADDRESS}</Text>
-        <Text>Faucet Address: {FAUCET_ADDRESS}</Text>
-        <Text>My Balance: {ethers.utils.formatEther(balance)}</Text>
-        <Text>Faucet Balance: {ethers.utils.formatEther(faucetBalance)}</Text>
-        <Button title="Refresh Balances" onPress={() => this.updateBalance()}/>
-        <Button title="Send 1 ETH to test faucet" onPress={() => this.sendTransaction("1.0", FAUCET_ADDRESS)}/>
-      </>
-    );
-  }
+      {isProcessing === true &&
+        <Text style={{color: "red"}}>Processing transaction...</Text>
+      } 
+      
+      {isProcessing === false &&
+        <Text style={{color: "green"}}>Transaction Completed.</Text>
+      }
+      <Text>Network: {network}</Text>
+      <Text>My Address: {MY_ADDRESS}</Text>
+      <Text>Faucet Address: {FAUCET_ADDRESS}</Text>
+      <Text>My Balance: {ethers.utils.formatEther(balance)}</Text>
+      <Text>Faucet Balance: {ethers.utils.formatEther(faucetBalance)}</Text>
+      {/* <Button title="Refresh Balances" onPress={() => this.updateBalance()}/> */}
+      <Button title="Send 1 ETH to test faucet" onPress={() => sendTransaction("1.0", FAUCET_ADDRESS)}/>
+    </>
+  );
 }
 
 const styles = StyleSheet.create({
